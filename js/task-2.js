b4cf04698ecf9797558e1c9ca7e9919dccce9eca
anton81951 <145291034+anton81951@users.noreply.github.com>
2024-01-10 08:54:49 +0200

    2

--- js/task-2.js
+++ js/task-2.js
@@ -1,6 +1,8 @@
+'use strict';
 function getShippingMessage(country, price, deliveryFee){
+    'use strict';
     console.log(`Shipping to ${country} will cost ${price+deliveryFee} credits`);
 }
-console.log(getShippingMessage("Australia", 120, 50)); // "Shipping to Australia will cost 170 credits"
-console.log(getShippingMessage("Germany", 80, 20)); // "Shipping to Germany will cost 100 credits"
-console.log(getShippingMessage("Sweden", 100, 20)); // "Shipping to Sweden will cost 120 credits"
+getShippingMessage("Australia", 120, 50);
+getShippingMessage("Germany", 80, 20);
+getShippingMessage("Sweden", 100, 20);
